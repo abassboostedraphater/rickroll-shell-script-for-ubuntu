a9cd74066910600296f34752dd854158f0ef87c1
abassboostedraphater <81880695+abassboostedraphater@users.noreply.github.com>
2021-04-05 14:52:30 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 sudo apt install mplayer
 sudo apt install wget
 wget https://download1507.mediafire.com/zmwwv2mxl3lg/g9njsn7xx5m5lp8/real.mp4
-  mplayer vid $HOME/real.mp4
+  mplayer $HOME/real.mp4
